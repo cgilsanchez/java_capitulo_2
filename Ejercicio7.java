791aea5986ea713200cd0a6e934c3ae377670411
cgilsanchez <cgilsanchez.02@gmail.com>
2022-10-12 13:34:04 +0200

    fin

---
+++ Ejercicio7.java
@@ -0,0 +1,7 @@
+public class Ejercicio7 {
+    public static void main(String[] args){
+        char letra1 = 'a';
+        String palabra1 = "comer";
+        System.out.println(letra1 + "" + palabra1);
+    }
+}
