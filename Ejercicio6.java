3a2034211d8496da25419e5f17910ad85fa77859
cgilsanchez <cgilsanchez.02@gmail.com>
2022-10-12 13:19:12 +0200

    fin

--- Ejercicio6.java
+++ Ejercicio6.java
@@ -1,9 +1,9 @@
 public class Ejercicio6 {
     public static void main(String[] args){
         double baseImponible = 80.00;
-        System.out.println("Base Imponible %8.2f",baseImponible);
-        System.out.println("IVA            %8.2f", (baseImponible*0.21));
-        System.out.println("------------------------");
-        System.out.println( "Total          %8.2f", (baseImponible*1.21));
+        System.out.printf("Base imponible %8.2f\n", baseImponible);
+        System.out.printf("IVA            %8.2f\n", (baseImponible * 0.21));
+        System.out.printf("-----------------------\n");
+        System.out.printf("Total          %8.2f\n", (baseImponible * 1.21));
     }
 }
